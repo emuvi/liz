0b567999092dd17b857b9511a68ad747d42a5b73
Éverton M. Vieira <everton.muvi@gmail.com>
2021-12-01 08:48:26 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), LizError> {
     for arg in std::env::args() {
         if arg == "-v" || arg == "--version" {
             let version = env!("CARGO_PKG_VERSION");
-            println!("Liz {}", version);
+            println!("Liz (LuaWizard) {}", version);
         } else if arg.ends_with(".lua") {
             match liz::execute(arg) {
                 Ok(result) => println!("{}", result),
