cca864919f229b512c3b0ba70e9bc9e93e2b5850
Éverton M. Vieira <everton.muvi@gmail.com>
2022-02-02 08:22:39 -0300

    add function git_has_changes on liz codes

--- src/wiz_codes.rs
+++ src/wiz_codes.rs
@@ -14,8 +14,13 @@ pub fn inject_codes<'a>(ctx: Context<'a>, liz: &Table<'a>) -> Result<(), LizErro
         utils::treat_error(ctx, liz_codes::git_is_ignored(&path))
     })?;
 
+    let git_has_changes = ctx.create_function(|ctx, path: String| {
+        utils::treat_error(ctx, liz_codes::git_has_changes(&path))
+    })?;
+
     liz.set("git_root_find", git_root_find)?;
     liz.set("git_is_ignored", git_is_ignored)?;
+    liz.set("git_has_changes", git_has_changes)?;
 
     Ok(())
 }
