66edf57d252ecd9ee934b43047f854267e5aea43
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-18 08:58:38 -0300

    updates the email

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ fn main() -> Result<(), LizError> {
 fn print_help() {
     println!(
         "liz {}
-Éverton M. Vieira <everton.muvi@gmail.com>
+Éverton M. Vieira <emuvi@outlook.com.br>
 
 Liz ( LuaWizard ) is a library and a command program that features a bunch of functionalities for lua scripts inside the liz global variable. 
     
